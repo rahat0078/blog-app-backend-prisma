8bbc2738661c592462e4c4c21e10f95ab02919f8
rahat0078 <rarahat1710@gmail.com>
2026-01-18 09:50:45 +0600

    all search and filter done

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,4 +1,4 @@
-import { Posts } from "../../../generated/prisma"
+import { Posts, PostStatus, Prisma } from "../../../generated/prisma"
 import { prisma } from "../../lib/prisma"
 
 type CreatePostInput = Omit<
@@ -15,8 +15,70 @@ const createPost = async (data: CreatePostInput, userId: string) => {
     })
 }
 
-const getAllPosts = async () => {
-    const result = await prisma.posts.findMany()
+const getAllPosts = async ({ search, tags, isFeatured, status, authorId }:
+    {
+        search: string | undefined,
+        tags: string[] | [],
+        isFeatured: boolean | undefined,
+        status: PostStatus | undefined, 
+        authorId: string | undefined,
+    }) => {
+    const andCondition: Prisma.PostsWhereInput[] = [];
+
+    if (search) {
+        andCondition.push(
+            {
+                OR: [
+                    {
+                        title: {
+                            contains: search as string,
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        content: {
+                            contains: search as string,
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        tags: {
+                            has: search as string
+                        }
+                    }
+                ]
+            }
+        )
+    }
+    if (tags.length > 0) {
+        andCondition.push(
+            {
+                tags: {
+                    hasEvery: tags as string[]
+                }
+            }
+        )
+    }
+
+    if (typeof isFeatured === 'boolean') {
+        andCondition.push({
+            isFeatured
+        })
+    }
+
+    if(status){
+        andCondition.push({status})
+    }
+    if(authorId){
+        andCondition.push({authorId})
+    }
+
+
+    const result = await prisma.posts.findMany({
+        where: {
+            AND: andCondition
+        }
+    })
     return result
 }
 
