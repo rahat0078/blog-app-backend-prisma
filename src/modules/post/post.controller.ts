2ce2f5654bff2d813653db2a1edf25b7225533fd
rahat0078 <rarahat1710@gmail.com>
2026-01-19 23:43:53 +0600

    pagination and sorting done

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -39,11 +39,17 @@ const getAllPosts = async (req: Request, res: Response) => {
                     : undefined
             : undefined;
 
-        const status = req.query.status as PostStatus | undefined
-        const authorId = req.query.authorId as string | undefined
+        const status = req.query.status as PostStatus | undefined;
+        const authorId = req.query.authorId as string | undefined;
+        const page = Number(req.query.page ?? 1);
+        const limit = Number(req.query.limit ?? 10);
+        const skip = (page- 1) * limit;
+        const sortBy = req.query.sortBy as string | undefined;
+        const sortOrder = req.query.sortOrder as string | undefined;
+        
 
 
-        const result = await postService.getAllPosts({ search: searchStr, tags, isFeatured, status, authorId });
+        const result = await postService.getAllPosts({ search: searchStr, tags, isFeatured, status, authorId, page, limit, skip, sortBy, sortOrder });
         res.status(200).json({
             success: true,
             message: "Post retrieved successfully",
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -15,13 +15,18 @@ const createPost = async (data: CreatePostInput, userId: string) => {
     })
 }
 
-const getAllPosts = async ({ search, tags, isFeatured, status, authorId }:
+const getAllPosts = async ({ search, tags, isFeatured, status, authorId, page, limit, skip, sortBy, sortOrder }:
     {
         search: string | undefined,
         tags: string[] | [],
         isFeatured: boolean | undefined,
         status: PostStatus | undefined, 
         authorId: string | undefined,
+        page: number,
+        limit: number,
+        skip: number,
+        sortBy: string | undefined,
+        sortOrder: string | undefined
     }) => {
     const andCondition: Prisma.PostsWhereInput[] = [];
 
@@ -75,8 +80,15 @@ const getAllPosts = async ({ search, tags, isFeatured, status, authorId }:
 
 
     const result = await prisma.posts.findMany({
+        take: limit,
+        skip,
         where: {
             AND: andCondition
+        },
+        orderBy: sortBy && sortOrder ? {
+            [sortBy]: sortOrder
+        } : {
+            createdAt: 'desc'
         }
     })
     return result
