cc91a264c644d652d4dbd6be858700e21a9560ba
rahat0078 <rarahat1710@gmail.com>
2026-01-26 23:11:58 +0600

    prisma client validation error

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,11 +1,20 @@
 import { NextFunction, Request, Response } from "express";
+import { Prisma } from "../../generated/prisma/client";
 
 function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
-    res.status(500)
+    let statusCode = 500;
+    let errorMessage = "Internal Server Error";
+
+    if(err instanceof Prisma.PrismaClientValidationError){
+        statusCode = 400;
+        errorMessage = "Missing fields or Incorrect field type";
+    }
+
+    res.status(statusCode)
     res.json({
-        message: 'error from error handler',
+        message: errorMessage,
         error: err
-    })
+    }) 
 }
 
 
