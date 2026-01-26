3384ddcf3bab01a1916eedc65c396f66a920ec32
rahat0078 <rarahat1710@gmail.com>
2026-01-26 23:46:16 +0600

    add prismaClient unknown, RustPanic, Initialization error in the global error middleware

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -4,12 +4,12 @@ import { Prisma } from "../../generated/prisma/client";
 function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
     let statusCode = 500;
     let errorMessage = "Internal Server Error";
-
+    // PrismaClientValidationError
     if (err instanceof Prisma.PrismaClientValidationError) {
         statusCode = 400;
         errorMessage = "Missing fields or Incorrect field type";
     }
-
+    // PrismaClientKnownRequestError
     else if (err instanceof Prisma.PrismaClientKnownRequestError) {
         if (err.code === "P2025") {
             statusCode = 404;
@@ -23,11 +23,26 @@ function errorHandler(err: any, req: Request, res: Response, next: NextFunction)
             statusCode = 400;
             errorMessage = "Foreign key constraint failed.";
         }
-        else{
+        else {
             statusCode = 400;
             errorMessage = "Invalid request data."
         }
-        
+
+    }
+
+    else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+        statusCode = 500;
+        errorMessage = "Something went wrong while processing your request."
+    }
+
+    else if (err instanceof Prisma.PrismaClientRustPanicError) {
+        statusCode = 503;
+        errorMessage = "The service is temporarily unavailable. Please try again later.";
+    }
+
+    else if (err instanceof Prisma.PrismaClientInitializationError) {
+        statusCode = 503;
+        errorMessage = "The service is currently unavailable. Please try again later.";
     }
 
     res.status(statusCode)
